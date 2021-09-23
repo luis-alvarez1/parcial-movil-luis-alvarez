6be7ebd982a79826d6a0d4f8999bd23502e7e02f
Luis <lealvarezp@unac.edu.co>
2021-09-23 01:56:44 -0500

    initial commit

---
+++ android/app/src/main/java/com/parcial_movil_luis_alvarez/MainActivity.java
@@ -0,0 +1,15 @@
+package com.parcial_movil_luis_alvarez;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "parcial_movil_luis_alvarez";
+  }
+}
