88564cfad6ab39ddee540678dafe0d57ab65e590
Luis <lealvarezp@unac.edu.co>
2021-09-28 01:48:54 -0500

    adding theming

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import App from './src/App';
 import { name as appName } from './app.json';
+import theme from './src/core/theme/theme';
 
 const Main = () => {
   return (
     <>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <App />
       </PaperProvider>
     </>
